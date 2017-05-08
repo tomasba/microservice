0120a9cd6e60960cdbeeddfe91642cfc1746e472
Laura Baltrukone <Laura Baltrukone>
2017-05-08 22:39:38 +0300

    add client conf

--- cloud/server/src/main/java/com/tomasba/server/ServerApplication.java
+++ cloud/server/src/main/java/com/tomasba/server/ServerApplication.java
@@ -2,10 +2,8 @@ package com.tomasba.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
